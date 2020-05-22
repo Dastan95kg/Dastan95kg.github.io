cf8784abee5e0c18b36407e70a7762283ff589d5
Dastan95kg <51588401+Dastan95kg@users.noreply.github.com>
2020-05-22 16:39:45 +0600

    Update index.js

--- learn-english-proj/js/index.js
+++ learn-english-proj/js/index.js
@@ -77,7 +77,7 @@ function reloadPage() {
 }
 
 // Close SVG
-const closeSVG = `<svg width="20px" height="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+const closeSVG = `<svg width="20px" height="20px" version="1.1" id="Layer_1" xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink" x="0px" y="0px"
 viewBox="0 0 492 492" style="enable-background:new 0 0 492 492;" xml:space="preserve">
    <path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872
        c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872
@@ -87,8 +87,8 @@ viewBox="0 0 492 492" style="enable-background:new 0 0 492 492;" xml:space="pres
        c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116
        c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"/>
 </svg>`;
-const burgerSVG = `<svg width="20px" height="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 469.333 469.333"
+const burgerSVG = `<svg width="20px" height="20px" version="1.1" id="Capa_1" xmlns="https://www.w3.org/2000/svg"
+xmlns:xlink="https://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 469.333 469.333"
 style="enable-background:new 0 0 469.333 469.333;" xml:space="preserve">
 <g>
     <path d="M53.333,106.667H416c29.417,0,53.333-23.927,53.333-53.333S445.417,0,416,0H53.333C23.917,0,0,23.927,0,53.333
